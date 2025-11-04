1b459ced9b81fa9307bf80a46b785492709780a2
Deodatus Bijengsi <124217738+Bichesq@users.noreply.github.com>
2025-11-04 16:38:57 +0000

    development on models

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WDS-backend-tutorial
 
 To get Ip address for server: curl https://ifconfig.me
+
+accessing codebase server via browser: https://<CODESPACE-NAME>-<PORT>.app.github.dev
+
+https://studious-parakeet-3000.app.github.dev
--- routes/authors.js
+++ routes/authors.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const Author = require('../models/author');
 const router = express.Router();
 
 // All authors
@@ -8,11 +9,11 @@ router.get('/', (req, res) => {
 
 //New authors   
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', {author: new Author()})
 })
 
 // Create new author
 router.post('/new', (req, res) => {
-    res.send("create")
+    res.send(req.body.name)
 })
 module.exports = router;
